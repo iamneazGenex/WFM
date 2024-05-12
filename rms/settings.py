bef43b765bd48cd861b349be96267f2cbd51479f
Al Amin Neaz Ahmed <alamin.neaz@genexinfosys.com>
2024-05-12 19:07:59 +0600

    updated settings.py to use mysql as database

--- rms/settings.py
+++ rms/settings.py
@@ -88,11 +88,22 @@ WSGI_APPLICATION = "rms.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# -------------------------------------------------------------
+# For SQL Lite 3
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+# -------------------------------------------------------------
+# For MySQL
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "/etc/mysql/my.cnf",
+        },
     }
 }
 
