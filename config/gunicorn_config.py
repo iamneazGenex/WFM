ad7ffe3d0b9fb40aba8959121d1943be8c593326
Al Amin Neaz Ahmed <alamin.neaz@genexinfosys.com>
2024-06-10 11:22:18 +0600

    added new gunicorn config

--- config/gunicorn_config.py
+++ config/gunicorn_config.py
@@ -5,16 +5,32 @@ from config.gunicorn_logging import configure_logging
 configure_logging()
 
 # Define Gunicorn configuration variables
+
+# bind = "unix:/run/gunicorn.sock"
+# workers = multiprocessing.cpu_count() * 2 + 1
+# limit_request_line = 0
+# timeout = 3600
+# graceful_timeout = 3600
+# keepalive = 3600
+# accesslog = "-"  # Redirect Gunicorn access logs to stdout
+# errorlog = "-"  # Redirect Gunicorn error logs to stdout
+# loglevel = "debug"
+# worker_class = "gevent"
+# max_requests = 1000  # Restart worker after processing 1000 requests
+# # Specify the working directory
+# WorkingDirectory = "/home/wfmuser/WFM"
+
 bind = "unix:/run/gunicorn.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 limit_request_line = 0
-timeout = 3600
-graceful_timeout = 3600
-keepalive = 3600
+timeout = 300  # Reduce timeout to a more reasonable value
+graceful_timeout = 300  # Reduce graceful timeout
+keepalive = 60  # Reduce keepalive to free up idle connections
 accesslog = "-"  # Redirect Gunicorn access logs to stdout
 errorlog = "-"  # Redirect Gunicorn error logs to stdout
 loglevel = "debug"
 worker_class = "gevent"
 max_requests = 1000  # Restart worker after processing 1000 requests
+max_requests_jitter = 100  # Add jitter to max_requests to spread out restarts
 # Specify the working directory
-WorkingDirectory = "/home/wfmuser/WFM"
+working_directory = "/home/wfmuser/WFM"
